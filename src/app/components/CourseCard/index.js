b4477401f6a9c5a7fd23cbc4fdd4e205ef2c61c1
chinmoyDn <chinmoy.debnath11@gmail.com>
2024-05-17 20:26:39 +0530

    checkpoint course-listing

---
+++ src/app/components/CourseCard/index.js
@@ -0,0 +1,99 @@
+import { StarIcon } from "@chakra-ui/icons";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  ButtonGroup,
+  Button,
+  Image,
+  Flex,
+  Box,
+  Spacer,
+  Icon,
+  HStack,
+} from "@chakra-ui/react";
+
+import ArrowRight from "@/app/components/icons/ArrowRight";
+
+export default function CourseCard({ course }) {
+  const {
+    title,
+    type,
+    duration,
+    author,
+    fees,
+    cashback,
+    rating,
+    thumbnail = "./course_thumbnails/course1.jpg",
+  } = course || {};
+  return (
+    <Card maxW="300px">
+      <CardBody p={0}>
+        <Box position="relative">
+          <Text
+            position="absolute"
+            top="2"
+            left="2"
+            bg="yellow.500"
+            p="2"
+            borderRadius="base"
+            style={{ fontWeight: 500 }}
+          >
+            ${cashback} cashback
+          </Text>
+          <Image
+            src={thumbnail}
+            borderRadius="base"
+            borderBottomRadius="none"
+            alt="course thumbnail"
+          />
+        </Box>
+        <Stack mt="6" spacing="2" p="2">
+          <Flex color="gray.600">
+            <Text>{type}</Text>
+            <Spacer />
+            <Text>{duration} weeks</Text>
+          </Flex>
+          <Text fontSize="large" style={{ fontWeight: "500" }}>
+            {title}
+          </Text>
+          <Text>By: {author}</Text>
+          <Flex>
+            <HStack spacing="1">
+              {typeof rating === "number" && (
+                <>
+                  <StarRatings rating={rating} color="yellow.500" />
+                  <StarRatings rating={5 - rating} color="gray.500" />
+                </>
+              )}
+            </HStack>
+            <Spacer />
+            <Text>$50</Text>
+          </Flex>
+        </Stack>
+      </CardBody>
+      <CardFooter p="2" style={{ fontWeight: 500 }}>
+        <Button
+          width="100%"
+          variant="solid"
+          rightIcon={<ArrowRight color={"white"} />}
+        >
+          Enroll Now
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+const StarRatings = ({ rating, color }) => {
+  console.log({ rating, color });
+  const stars = [...Array(rating).keys()];
+  return stars.map((item) => (
+    <Icon key={`ratings-${item}`} as={StarIcon} color={color} />
+  ));
+};
---
+++ src/app/components/CourseExplorer/index.js
@@ -0,0 +1,109 @@
+import {
+  Box,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  SimpleGrid,
+} from "@chakra-ui/react";
+
+import CourseCard from "@/app/components/CourseCard";
+
+export default function CourseExplorer() {
+  const popularCourses = courses.filter(({ tags }) => {
+    return tags.includes("popular");
+  });
+
+  const newCourses = courses.filter(({ tags = [] }) => {
+    return tags.includes("new");
+  });
+
+  return (
+    <Tabs width="100%">
+      <TabList>
+        <Tab>All</Tab>
+        <Tab>Most Popular</Tab>
+        <Tab>New</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          <SimpleGrid templateColumns="repeat(4, 1fr)" gap={6}>
+            {courses.map((course, i) => (
+              /* <Box > */
+              <CourseCard course={course} key={`course-index-${i}`} />
+              /* </Box> */
+            ))}
+          </SimpleGrid>
+        </TabPanel>
+        <TabPanel>
+          <SimpleGrid templateColumns="repeat(4, 1fr)" gap={6}>
+            {popularCourses.map((course, i) => (
+              /* <Box > */
+              <CourseCard course={course} key={`course-index-${i}`} />
+              /* </Box> */
+            ))}
+          </SimpleGrid>
+        </TabPanel>
+        <TabPanel>
+          <SimpleGrid templateColumns="repeat(4, 1fr)" gap={6}>
+            {newCourses.map((course, i) => (
+              /* <Box > */
+              <CourseCard course={course} key={`course-index-${i}`} />
+              /* </Box> */
+            ))}
+          </SimpleGrid>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+}
+
+const courses = [
+  {
+    title: "Become a Certified Web Developer: HTML, CSS and JavaScript",
+    type: "development",
+    duration: "16",
+    author: "Wisdom Umanah",
+    fees: "50",
+    cashback: "20",
+    rating: 4,
+    thumbnail: "./course_thumbnails/course1.jpg",
+    tags: ["new"],
+  },
+
+  {
+    title: "Javascript for Beginners",
+    type: "development",
+    duration: "16",
+    author: "Mayank",
+    fees: "30",
+    cashback: "10",
+    rating: 5,
+    thumbnail: "./course_thumbnails/course2.jpg",
+    tags: ["popular", "new"],
+  },
+  {
+    title: "Become a Certified Web Developer: HTML, CSS and JavaScript",
+    type: "development",
+    duration: "16",
+    author: "Sudhanshu Kumar",
+    fees: "50",
+    cashback: "20",
+    rating: 5,
+    thumbnail: "./course_thumbnails/course3.jpg",
+    tags: ["popular", "new"],
+  },
+  {
+    title: "Become a Certified Web Developer: HTML, CSS and JavaScript",
+    type: "development",
+    duration: "16",
+    author: "Chinmoy and Alex",
+    fees: "50",
+    cashback: "20",
+    rating: 3,
+    thumbnail: "./course_thumbnails/course4.jpg",
+    tags: [],
+  },
+];
